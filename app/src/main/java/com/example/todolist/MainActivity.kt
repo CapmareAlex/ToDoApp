4060fe40fb2949270d95508e6d265b41d8154ede
Capmare Alex-Mihai <73711932+CapmareAlex@users.noreply.github.com>
2023-05-20 22:27:47 +0300

    First commit.

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.todolist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener {
+            NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
+        }
+
+
+    }
+}
